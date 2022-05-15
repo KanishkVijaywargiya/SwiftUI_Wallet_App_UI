f82973e551e20256c3b7d14d6e5126a4ec9c590f
KanishkVijaywargiya <secretcodersociety@gmail.com>
2022-05-15 13:00:30 +0530

    Initial Commit

---
+++ WalletAppUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WalletAppUI
+//
+//  Created by Kanishk Vijaywargiya on 15/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WalletAppUI/WalletAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAppUIApp.swift
+//  WalletAppUI
+//
+//  Created by Kanishk Vijaywargiya on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WalletAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
